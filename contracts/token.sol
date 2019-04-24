a19737452160254b47967d1104bf299974dbbe74
Luis F. Escobar-Driver <lfescoba@uci.edu>
2019-04-24 13:25:57 -0700

    init set up. Building on erc1155 base

---
+++ contracts/token.sol
@@ -0,0 +1,12 @@
+pragma solidity >=0.4.21 <0.6.0;
+
+import "../libs/ERC1155.sol";
+import "../libs/ERC1155Mintable.sol";
+
+contract GameToken is ERC1155, ERC1155Mintable {
+    
+    constructor() public {
+
+    }
+
+}
